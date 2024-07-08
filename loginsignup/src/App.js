abc55b17985f19aed41d8ba6b1c6ca57d918346b
Vikas <vikasprajapati1812@gmail.com>
2024-07-08 23:03:56 +0530

    loginSignup

---
+++ loginsignup/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginSignup from './components/LoginSingup/LoginSignup';
+
+function App() {
+  return (
+    <div >
+     <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
